13c3f3f90248e5220c75fa1a3497f18d01cc3e69
Richou <richou6@gmail.com>
2020-10-23 22:47:46 +0200

    Last step: combine providers

--- src/app/app.contexts.jsx
+++ src/app/app.contexts.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import { CodesProvider } from './codes/context/codes.context'
 import { RecipesProvider } from "./recipes/context/recipes.context";
 
+function combine(...providers) {
+  if (providers.length === 1) {
+    return providers[0]
+  }
+
+  return providers.reduce((Provider1, Provider2) => ({ children }) => <Provider1><Provider2>{children}</Provider2></Provider1>)
+}
+
+const AppProviders = combine(
+  CodesProvider,
+  RecipesProvider
+)
+
 function AppContexts({ children }) {
   return (
-    <CodesProvider>
-      <RecipesProvider>
-        {children}
-      </RecipesProvider>
-    </CodesProvider>
+    <AppProviders>
+      {children}
+    </AppProviders>
   )
 }
 
